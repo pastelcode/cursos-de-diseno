328865dc0a3653792e1173f0432293c8d1f60825
Pastel <pastel.dev.gt@gmail.com>
2022-07-16 14:32:38 -0600

    Re build application depending on user authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,49 @@
-import 'package:curso_de_diseno/ui/pages/home/home_page.dart';
+import 'package:curso_de_diseno/data/repositories/shared_preferences_repository.dart';
+import 'package:curso_de_diseno/domain/cubit/user_cubit.dart';
+import 'package:curso_de_diseno/ui/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:routemaster/routemaster.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'domain/entities/user.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferencesRepository.init();
+  runApp(const MyAppStateProvider());
+}
+
+class MyAppStateProvider extends StatelessWidget {
+  const MyAppStateProvider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => UserCubit(),
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: RoutemasterDelegate(
-        routesBuilder: (context) => RouteMap(routes: {
-          '/': (_) => const MaterialPage(child: HomePage()),
-        }),
-      ),
-      routeInformationParser: const RoutemasterParser(),
+    final user = BlocProvider.of<UserCubit>(context);
+    user.checkForLoggedInUser();
+
+    return BlocBuilder<UserCubit, UserState>(
+      builder: (context, state) {
+        return MaterialApp.router(
+          routerDelegate: RoutemasterDelegate(
+            routesBuilder: (BuildContext context) {
+              return user.isLoggedIn ? loggedInMap : loggedOutMap;
+            },
+          ),
+          routeInformationParser: const RoutemasterParser(),
+        );
+      },
     );
   }
 }
