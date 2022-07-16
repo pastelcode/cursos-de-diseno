0500bc15fe52e7a651ea709784d4907b4f3b7208
Pastel <pastel.dev.gt@gmail.com>
2022-07-16 15:19:04 -0600

    Re design of login page

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -28,42 +28,65 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final user = BlocProvider.of<UserCubit>(context);
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Form(
-            key: _formKey,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Sandra Rosales',
-                    border: OutlineInputBorder(),
-                    labelText: 'Nombre de usuario',
+          Text(
+            'Cursos de diseño',
+            style: Theme.of(context).textTheme.displayMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 55,
+          ),
+          SizedBox(
+            width: 300,
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Sandra Rosales',
+                      border: OutlineInputBorder(),
+                      labelText: 'Nombre de usuario',
+                    ),
+                    textCapitalization: TextCapitalization.words,
+                    controller: _textFieldController,
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor escribe tu nombre';
+                      }
+                      if (value.length < 2) {
+                        return 'Tu nombre de usuario debe tener al menos 2 caracteres';
+                      }
+                      return null;
+                    },
                   ),
-                  textCapitalization: TextCapitalization.words,
-                  controller: _textFieldController,
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor escribe tu nombre';
-                    }
-                    if (value.length < 2) {
-                      return 'Tu nombre de usuario debe tener al menos 2 caracteres';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      user.login(name: _textFieldController.text);
-                    }
-                  },
-                  child: const Text('Ingresar'),
-                ),
-              ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        user.login(name: _textFieldController.text);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    child: const Text(
+                      'Ingresar',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
