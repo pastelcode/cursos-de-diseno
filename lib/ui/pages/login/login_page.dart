41aa949d2e19a698918c0e981bc3d2ef80995ef6
Pastel <pastel.dev.gt@gmail.com>
2022-07-16 14:32:18 -0600

    Create login page

---
+++ lib/ui/pages/login/login_page.dart
@@ -0,0 +1,73 @@
+import 'package:curso_de_diseno/domain/cubit/user_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late TextEditingController _textFieldController;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _textFieldController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final user = BlocProvider.of<UserCubit>(context);
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Sandra Rosales',
+                    border: OutlineInputBorder(),
+                    labelText: 'Nombre de usuario',
+                  ),
+                  textCapitalization: TextCapitalization.words,
+                  controller: _textFieldController,
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor escribe tu nombre';
+                    }
+                    if (value.length < 2) {
+                      return 'Tu nombre de usuario debe tener al menos 2 caracteres';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      user.login(name: _textFieldController.text);
+                    }
+                  },
+                  child: const Text('Ingresar'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
