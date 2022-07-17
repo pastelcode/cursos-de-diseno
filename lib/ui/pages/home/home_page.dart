40d3069ca8a149ce0c1e2456cbce65f690ea0a2e
Pastel <pastel.dev.gt@gmail.com>
2022-07-17 01:12:01 -0600

    Add courses list to home page

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,15 +1,28 @@
+import 'package:curso_de_diseno/data/repositories/courses_repository.dart';
+import 'package:curso_de_diseno/ui/pages/home/widgets/courses_list.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/my_app_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    CoursesRepository.init();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: MyAppBar(),
-      body: Text('Hola'),
+      body: CoursesList(),
     );
   }
 }
