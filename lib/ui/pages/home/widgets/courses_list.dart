bb426b62ecae066aaf59f75b63cacc870a67f66a
Pastel <pastel.dev.gt@gmail.com>
2022-07-17 23:00:26 -0600

    Create courses list tile design

--- lib/ui/pages/home/widgets/courses_list.dart
+++ lib/ui/pages/home/widgets/courses_list.dart
@@ -1,5 +1,9 @@
+import 'package:boxicons/boxicons.dart';
 import 'package:curso_de_diseno/data/repositories/courses_repository.dart';
+import 'package:curso_de_diseno/domain/entities/course.dart';
+import 'package:curso_de_diseno/ui/services/scaffold_messenger_service.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../domain/entities/module.dart';
 
@@ -25,15 +29,43 @@ class _CoursesListState extends State<CoursesList> {
             ExpansionPanel(
               canTapOnHeader: true,
               isExpanded: module.isExpandedOnList,
-              headerBuilder: (_, __) => ListTile(
+              headerBuilder: (_, bool isExpanded) => ListTile(
                 title: Text(module.title),
+                subtitle: isExpanded ? null : Text(module.description),
               ),
-              body: ListTile(
-                title: Text(module.description),
+              body: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 16.0,
+                      bottom: 10.0,
+                    ),
+                    child: Text(module.description),
+                  ),
+                  for (Course course in module.courses)
+                    ListTile(
+                      title: Text(course.title),
+                      iconColor: Colors.red,
+                      leading: const Icon(Boxicons.bxl_youtube),
+                      onTap: () async {
+                        _launchUrl(course.youtubeLink);
+                      },
+                    ),
+                ],
               ),
             )
         ],
       ),
     );
+  }
+
+  Future<void> _launchUrl(String url) async {
+    final uri = Uri.parse(url);
+    try {
+      launchUrl(uri);
+    } catch (e) {
+      SnackBarService.showErrorOnLaunchingUrlSnackBar();
+    }
   }
 }
